a7aec2e711aba9ec4e638f83d907ab48b56c7980
Lucas Teixeira <floxlucas@gmail.com>
2024-07-17 18:32:48 -0300

    adição de token para login

--- pages/login.js
+++ pages/login.js
@@ -4,17 +4,54 @@ import Input from "../src/components/input/input"
 import Button from "../src/components/button/button"
 import Link from "next/link"
 import Image from "next/image"
+import { setCookie } from 'cookies-next'
+import { useRouter } from 'next/router'
+import {useState} from 'react'
+
 
 export default function LoginPage(){
-    return(
+    const[formData, setFormData] = useState({
+        email: '',
+        password: '',    
+     })
+
+    const [error, setError] = useState('')
+    const router = useRouter()
+    
+    const handleFormEdit = (event, name) => {
+        setFormData({
+            ...formData,
+            [name]: event.target.value
+        })
+    }
+
+    const handleForm = async (event) => {
+        try {
+            event.preventDefault()
+            const response = await fetch('api/user/login', {
+                method: 'POST',
+                body: JSON.stringify(formData)
+            })
+            const json = await response.json()
+            if (response.status !== 200) throw new Error(json)
+            
+            setCookie('authorization', json)
+            router.push('/')
+        } catch(err){
+            setError(err.message)
+        }
+    }
+
+     return(
         <div className={styles.background}>
             <Link href="/register"><Button className={styles.novaConta}>Criar conta</Button></Link>
             <LoginCard nomeEmpresa="DOM CHICO BARBEARIA" title="Login">
-                <form className={styles.form}>
-                    <Input type="email" placeholder="Digite o e-mail cadastrado"/>
-                    <Input type="password" placeholder="Digite sua senha"/>
+                <form className={styles.form} onSubmit={handleForm}>
+                    <Input type="email" placeholder="Digite o e-mail cadastrado" required value={formData.email} onChange={(e) => {handleFormEdit(e, 'email')}}/>
+                    <Input type="password" placeholder="Digite sua senha" required value={formData.password} onChange={(e) => {handleFormEdit(e, 'password')}}/>
                     <Button>Entrar</Button>
                     <Link href="#" className={styles.esquece}>Esqueceu sua senha?</Link>
+                    {error && <p className={styles.error}>{error}</p>}
                 </form>
             </LoginCard>
         </div>
--- pages/register.js
+++ pages/register.js
@@ -1,19 +1,57 @@
+import {useState} from 'react'
 import styles from "../styles/Register.module.css"
 import LoginCard from "../src/components/loginCard/loginCard"
 import Input from "../src/components/input/input"
 import Button from "../src/components/button/button"
 import Link from "next/link"
+import { setCookie } from 'cookies-next'
+import { useRouter } from 'next/router'
 
 export default function RegisterPage(){
+    const[formData, setFormData] = useState({
+       name: '',
+       email: '',
+       tel: '',
+       password: '',    
+    })
+
+    const [error, setError] = useState('')
+    const router = useRouter()
+
+    const handleFormEdit = (event, name) => {
+        setFormData({
+            ...formData,
+            [name]: event.target.value
+        })
+    }
+
+    const handleForm = async (event) => {
+        try {
+            event.preventDefault()
+            const response = await fetch('api/user/register', {
+                method: 'POST',
+                body: JSON.stringify(formData)
+            })
+            const json = await response.json()
+            if (response.status !== 201) throw new Error(json)
+            
+            setCookie('authorization', json)
+            router.push('/')
+        } catch(err){
+            setError(err.message)
+        }
+    }
+
     return(
         <div className={styles.background}>
             <LoginCard nomeEmpresa="DOM CHICO BARBEARIA" title="Cadastro">
-                <form className={styles.form}>
-                    <Input type="text" placeholder="Nome completo"/>
-                    <Input type="email" placeholder="E-mail"/>
-                    <Input type="password" placeholder="Celular"/>
-                    <Input type="password" placeholder="Senha"/>
+                <form  onSubmit={handleForm} className={styles.form}>
+                    <Input type="text" placeholder="Nome completo" required value={formData.name} onChange={(e) => {handleFormEdit(e, 'name')}}/>
+                    <Input type="email" placeholder="E-mail" required value={formData.email} onChange={(e) => {handleFormEdit(e, 'email')}}/>
+                    <Input type="tel" placeholder="Celular" required value={formData.tel} onChange={(e) => {handleFormEdit(e, 'tel')}}/>
+                    <Input type="password" placeholder="Senha" required value={formData.password} onChange={(e) => {handleFormEdit(e, 'password')}}/>
                     <Button>Cadastrar</Button>
+                    {error && <p className={styles.error}>{error}</p>}
                     <Link href="/login" className={styles.return}>Já possui uma conta?</Link>
                 </form>
             </LoginCard>
